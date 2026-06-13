d52926fb419bf259750ab7a793098d2d880c069f
Sơn <sonvu2706@gmail.com>
2026-06-14 01:50:35 +0700

    firt commit

---
+++ src/data/videos.ts
@@ -0,0 +1,28 @@
+import { Video } from "../types/video";
+
+export const videos: Video[] = [
+    {
+        id: 1,
+        videoUrl:
+            "https://www.w3schools.com/html/mov_bbb.mp4",
+        authorName: "Nguyen Van A",
+        description: "Video demo 1",
+        likesCount: 100,
+    },
+    {
+        id: 2,
+        videoUrl:
+            "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/friday.mp4",
+        authorName: "Tran Van B",
+        description: "Video demo 2",
+        likesCount: 200,
+    },
+    {
+        id: 3,
+        videoUrl:
+            "https://media.w3.org/2010/05/sintel/trailer.mp4",
+        authorName: "Le Van C",
+        description: "Video demo 3",
+        likesCount: 300,
+    },
+];
---
+++ src/types/video.ts
@@ -0,0 +1,7 @@
+export interface Video {
+    id: number;
+    videoUrl: string;
+    authorName: string;
+    description: string;
+    likesCount: number;
+}
